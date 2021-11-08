5a625a0208346509f11fc4db96916e560976bdf4
Misha Kalita <mishakalita3@gmail.com>
2021-11-08 22:06:37 +0200

    test celery

--- swipe/settings.py
+++ swipe/settings.py
@@ -163,8 +163,8 @@ AUTH_USER_MODEL = 'account.User'
 
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'mishakalita1@gmail.com'
-EMAIL_HOST_PASSWORD = 'Monsterfun1234'
+EMAIL_HOST_USER = os.environ.get('EMAIL')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASSWORD')
 EMAIL_PORT = 587
 
 
