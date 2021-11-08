caa82436920a4c0ebeebf9852fc15d3a3869afda
Misha Kalita <mishakalita3@gmail.com>
2021-11-08 20:55:07 +0200

    test celery

--- building/tasks.py
+++ building/tasks.py
@@ -8,6 +8,7 @@ from celery.schedules import crontab
 
 @app.on_after_finalize.connect
 def setup_periodic_tasks(sender, **kwargs):
+    sender.add_periodic_task(10.0, test.s('Hello'))
 
     sender.add_periodic_task(
         crontab(hour=1, minute=0),
