25b8546a0852ba110d47ca6112daca5581222a80
Misha Kalita <mishakalita3@gmail.com>
2021-11-09 20:45:55 +0200

    add avatar field to userlistserializer

--- account/serializers.py
+++ account/serializers.py
@@ -18,7 +18,7 @@ class UserListSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ('id', 'first_name', 'last_name', 'email', 'role')
+        fields = ('id', 'first_name', 'last_name', 'email', 'role', 'avatar')
 
 
 class RecursiveSerializer(serializers.Serializer):
